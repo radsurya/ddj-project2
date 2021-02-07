2045f0c235d947a117f1b0278589474a818528f6
manueltomas <manuel.tomas1@outlook.com>
2021-02-07 16:20:34 +0000

    pause menu added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+            if(GameIsPaused){
+                switch (MainMenuArrowControl.pos){
+                    case 1: //PLAY
+                        Resume();
+                        break;
+                    case 2: //QUIT
+                        Quit();
+                        break;
+                    
+                }
+            }
+        }
     }
 
-    public void startGame()
-    {
-        GameManager.ChangeScene(GameManager.levelScene);
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 
-    public void quitGame()
-    {
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
 
+    public void Quit(){
+        #if UNITY_EDITOR
+        
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        #endif
+        Application.Quit();
     }
 }
